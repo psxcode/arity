7e08c4d7d74f84e0ebb392836be5b8efafde7868
psxcode <psxcode@gmail.com>
2018-08-05 22:22:00 +0200

    fix branch

--- src/branch.spec.ts
+++ src/branch.spec.ts
@@ -3,8 +3,44 @@ import branch from './branch'
 
 describe('[ branch ]', () => {
   it('should work', async () => {
-    const br = branch(x => x === 1, (x: number) => x, (x: number) => x * 2)
+    const br = branch(
+      (x: number) => x === 1,
+      (x: number) => x,
+      (x: number) => x * 2
+    )
     expect(br(1)).eq(1)
     expect(br(2)).eq(4)
+  })
+
+  it('should work', async () => {
+    let res = true
+    const br = branch(
+      () => res,
+      () => 1,
+      () => 4
+    )
+    expect(br()).eq(1)
+    res = false
+    expect(br()).eq(4)
+  })
+
+  it('should work', async () => {
+    const br = branch(
+      (x: number) => x === 1,
+      (x: number) => x
+    )
+    expect(br(1)).eq(1)
+    expect(br(2)).eq(undefined)
+  })
+
+  it('should work', async () => {
+    let res = true
+    const br = branch(
+      () => res,
+      () => 2
+    )
+    expect(br()).eq(2)
+    res = false
+    expect(br()).eq(undefined)
   })
 })
--- src/branch.ts
+++ src/branch.ts
@@ -1,4 +1,9 @@
-const branch = <T, R1, R2> (pred: (arg?: T) => boolean, fn0: (p?: T) => R1, fn1: (p?: T) => R2) =>
-  (arg?: T) => pred(arg) ? fn0(arg) : fn1(arg)
+function branch<R0, R1> (pred: () => boolean, fn0: () => R0, fn1: () => R1): () => R0 | R1
+function branch<T, R0, R1> (pred: (arg: T) => boolean, fn0: (p: T) => R0, fn1: (p: T) => R1): (arg: T) => R0 | R1
+function branch<R> (pred: () => boolean, fn: () => R): () => R | undefined
+function branch<T, R> (pred: (arg: T) => boolean, fn: (p: T) => R): (arg: T) => R | undefined
+function branch (pred: any, ...fns: any[]) {
+  return (arg: any) => pred(arg) ? fns[0](arg) : fns[1] ? fns[1](arg) : void 0
+}
 
 export default branch
