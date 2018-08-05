9e01103e7028c05aa68f6c04af3ba5eabe16adb1
psxcode <psxcode@gmail.com>
2018-08-05 22:01:16 +0200

    fix spread

--- src/spread.spec.ts
+++ src/spread.spec.ts
@@ -2,7 +2,12 @@ import spread from './spread'
 
 describe('[ spread ]', () => {
   it('should work', () => {
-    const spy = (a: number, b: string, c: boolean) => 42
+    const spy = (a: number, b: string, c: boolean) => {
+      void a
+      void b
+      void c
+      return 42
+    }
     const f = spread(spy)
     f([1, 'a', true])
   })
