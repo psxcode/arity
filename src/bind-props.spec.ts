db4175624a1646089938810907f438b8fa93737c
psxcode <psxcode@gmail.com>
2018-08-05 19:40:59 +0200

    fix bind-props

--- src/bind-props.spec.ts
+++ src/bind-props.spec.ts
@@ -1,15 +1,10 @@
 import { expect } from 'chai'
 import bindProps from './bind-props'
 
-const id = (value: any) => value
-const addNamed = (a: string, b: string) => (p: {[k: string]: any}) => p[a] + p[b]
+const addNamed = <S0 extends string, S1 extends string>(a: S0, b: S1) =>
+  (p: { [k in S0 | S1]: number }) => p[a] + p[b]
 
 describe('[ bindProps ]', () => {
-  it('should work as a constant', () => {
-    const binded = bindProps({ value0: 0 })(id)
-    expect(binded()).deep.eq({ value0: 0 })
-  })
-
   it('should work', () => {
     const binded = bindProps({ val0: 5 })(addNamed('val0', 'val1'))
     expect(binded({ val1: 10 })).eq(15)
--- src/bind-props.ts
+++ src/bind-props.ts
@@ -1,4 +1,6 @@
-const bindProps = (props0: IHash<any>) =>
-  (fn: PositionalFn<any>) => (props1: IHash<any> = {}) => fn({ ...props0, ...props1 })
+const bindProps = <P0 extends {}> (p0: P0) =>
+  <A0 extends P0, R> (fn: (props: A0) => R) =>
+    (p1: {[k in Exclude<keyof A0, keyof P0>]: A0[k]}) =>
+      fn(Object.assign({}, p0, p1) as A0)
 
 export default bindProps
