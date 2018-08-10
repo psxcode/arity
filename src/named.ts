c0cbf40b1c69f8f32f4ca3035c81edbd779d919e
alex.feinstein <alex.feinstein@klarna.com>
2018-08-10 10:25:31 +0200

    remove named and positional

--- src/named.ts
+++
@@ -1,9 +0,0 @@
-const named = (keys: () => string[]) =>
-  <R> (fn: (props: {[k: string]: any}) => R) =>
-    (...args: any[]) =>
-      fn(keys().reduce((res: {[k: string]: any}, name, i) => {
-        res[name] = args[i]
-        return res
-      }, {}))
-
-export default named
--- src/positional.ts
+++
@@ -1,6 +0,0 @@
-const positional = (keys = Object.keys) =>
-  (fn: PositionalFn<any>) =>
-    (props: IHash<any>) =>
-      fn(...keys(props).map(name => props[name]))
-
-export default positional
